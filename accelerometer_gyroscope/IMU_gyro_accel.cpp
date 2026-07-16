bf13f9fc84ba926f383c27d5d9098a0f96e725a6
alexgruden <agruden@uvm.edu>
2026-07-16 09:31:52 -0400

    feat: initialize serial communication and I2C transmission in IMU setup

--- accelerometer_gyroscope/IMU_gyro_accel.cpp
+++ accelerometer_gyroscope/IMU_gyro_accel.cpp
@@ -11,3 +11,26 @@ float roll, pitch, yaw;
 float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
 float elapsedTime, currentTime, previousTime;
 int c = 0;
+
+void setup() {
+    Serial.begin(19200); // Twice normal buad rate
+
+    Wire.begin();
+    Wire.beginTransmission(MPU_ADDR);
+
+    Wire.write(0x6B);                  // Talk to the register 6B
+    Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
+
+    Wire.endTransmission(true);
+
+    calculate_IMU_error();
+    delay(20);
+}
+
+void loop () {
+
+}
+
+void calculate_imu_error () {
+    
+}
